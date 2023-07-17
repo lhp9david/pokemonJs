c203dc8a0a157ded2d83517cf0d97fdbc9d66836
David Godard <davidgodard76700@gmail.com>
2023-07-17 18:45:17 +0200

    maj

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let image = document.querySelector('.image');
 let pokeName = document.querySelector('.name');
 let type = document.querySelector('.type');
 let id = document.querySelector('.id');
+let evolution = document.querySelector('.next');
+let imageEvo = document.querySelector('.imageEvo');
 
 
 
@@ -19,6 +21,17 @@ let getPokemon = async () => {
     pokeName.innerHTML = response.name.fr;
     type.innerHTML = response.types[0].name;
     id.innerHTML = response.pokedexId;
+    if (response.evolution.next === null) {
+        evolution.innerHTML = 'Aucune évolution';
+        imageEvo.src = '../img/annuler.png';
+    } else {
+        evolution.innerHTML = response.evolution.next[0].name;
+        let url2 = `https://api-pokemon-fr.vercel.app/api/v1/pokemon/${response.evolution.next[0].pokedexId}`;
+        let data2 = await fetch(url2);
+        let response2 = await data2.json();
+        imageEvo.src = response2.sprites.shiny;
+        
+    }
         
 }
 
